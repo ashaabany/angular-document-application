661b99c76921c06052f177e81e460219fc8fbb68
Ahmed Shaaban <ahmed.shaaban@avanade.com>
2022-12-11 23:05:32 -0800

    added json data

--- src/app/search-form/search-form.component.ts
+++ src/app/search-form/search-form.component.ts
@@ -1,4 +1,17 @@
 import { Component } from '@angular/core';
+import entityData from './entityData.json';
+
+export interface Entity {
+  "Animals": Array<Information>;
+  "Plants": Array<Information>;
+  "Minerals": Array<Information>;
+}
+export interface Information {
+  name: string;
+  class: string;
+}
+
+const entities: Entity = entityData
 
 @Component({
   selector: 'app-search-form',
@@ -6,5 +19,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./search-form.component.scss']
 })
 export class SearchFormComponent {
+  
+  entities: Entity = entityData
+
+  ngOnInit() {
+    console.log(this.entities["Animals"]);
+  }
 
 }
